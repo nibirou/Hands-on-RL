2083aef8bb3bb8db5f20caebac2b4249bf8d8a3f
nibirou <741420370@qq.com>
2025-06-17 07:36:30 +0000

    更新

---
+++ Chapter3/Chapter3-2.py
@@ -0,0 +1,27 @@
+# 求解马尔可夫价值函数解析解
+import numpy as np
+np.random.seed(0)
+# 定义状态转移概率矩阵P
+P = [
+    [0.9, 0.1, 0.0, 0.0, 0.0, 0.0],
+    [0.5, 0.0, 0.5, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.6, 0.0, 0.4],
+    [0.0, 0.0, 0.0, 0.0, 0.3, 0.7],
+    [0.0, 0.2, 0.3, 0.5, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+]
+P = np.array(P)
+
+
+def compute(P, rewards, gamma, states_num):
+    ''' 利用贝尔曼方程的矩阵形式计算解析解,states_num是MRP的状态数 '''
+    rewards = np.array(rewards).reshape((-1, 1))  #将rewards写成列向量形式
+    value = np.dot(np.linalg.inv(np.eye(states_num, states_num) - gamma * P),
+                   rewards)
+    return value
+
+rewards = [-1, -2, -2, 10, 1, 0]  # 定义奖励函数
+gamma = 0.5  # 定义折扣因子
+
+V = compute(P, rewards, gamma, 6)
+print("MRP中每个状态价值分别为\n", V)
