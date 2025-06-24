a4cfd7f0d9c95241207bdc2b6df9704cb35360c8
nibirou <741420370@qq.com>
2025-06-24 22:56:33 +0800

    更新

--- Chapter4/Chapter4-1.py
+++ Chapter4/Chapter4-1.py
@@ -36,3 +36,36 @@ class CliffWalkingEnv:
                     P[i * self.ncol + j][a] = [(1, next_state, reward, done)]
         return P
 
+class PolicyIteration:
+    """ 策略迭代算法 """
+    def __init__(self, env, theta, gamma):
+        self.env = env
+        self.v = [0] * self.env.ncol * self.env.nrow  # 初始化价值为0
+        self.pi = [[0.25, 0.25, 0.25, 0.25]
+                   for i in range(self.env.ncol * self.env.nrow)]  # 初始化为均匀随机策略
+        self.theta = theta  # 策略评估收敛阈值
+        self.gamma = gamma  # 折扣因子
+    
+    def policy_evaluation(self):  # 策略评估
+        cnt = 1  # 计数器
+        while 1:
+            max_diff = 0
+            new_v = [0] * self.env.ncol * self.env.nrow
+            for s in range(self.env.ncol * self.env.nrow):
+                qsa_list = []  # 开始计算状态s下的所有Q(s,a)价值
+                for a in range(4):
+                    qsa = 0
+                    for res in self.env.P[s][a]:
+                        p, next_state, r, done = res
+                        qsa += p * (r + self.gamma * self.v[next_state] *
+                                    (1 - done))
+                        # 本章环境比较特殊,奖励和下一个状态有关,所以需要和状态转移概率相乘
+                    qsa_list.append(self.pi[s][a] * qsa)
+                new_v[s] = sum(qsa_list)  # 状态价值函数和动作价值函数之间的关系
+                max_diff = max(max_diff, abs(new_v[s] - self.v[s]))
+            self.v = new_v
+            if max_diff < self.theta: break  # 满足收敛条件,退出评估迭代
+            cnt += 1
+        print("策略评估进行%d轮后完成" % cnt)
+    
+    
