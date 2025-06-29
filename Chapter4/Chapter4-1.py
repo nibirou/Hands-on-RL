07091f5ce179bb442513957795715d82aad6920f
nibirou <741420370@qq.com>
2025-06-29 22:31:13 +0800

    更新

--- Chapter4/Chapter4-1.py
+++ Chapter4/Chapter4-1.py
@@ -68,4 +68,63 @@ class PolicyIteration:
             cnt += 1
         print("策略评估进行%d轮后完成" % cnt)
     
-    
+    def policy_improvement(self):  # 策略提升
+        for s in range(self.env.nrow * self.env.ncol):
+            qsa_list = []
+            for a in range(4):
+                qsa = 0
+                for res in self.env.P[s][a]:
+                    p, next_state, r, done = res
+                    qsa += p * (r + self.gamma * self.v[next_state] *
+                                (1 - done))
+                qsa_list.append(qsa)
+            maxq = max(qsa_list)
+            cntq = qsa_list.count(maxq)  # 计算有几个动作得到了最大的Q值
+            # 让这些动作均分概率
+            self.pi[s] = [1 / cntq if q == maxq else 0 for q in qsa_list]
+        print("策略提升完成")
+        return self.pi
+
+    def policy_iteration(self):  # 策略迭代
+        while 1:
+            self.policy_evaluation()
+            old_pi = copy.deepcopy(self.pi)  # 将列表进行深拷贝,方便接下来进行比较
+            new_pi = self.policy_improvement()
+            if old_pi == new_pi: break
+            
+def print_agent(agent, action_meaning, disaster=[], end=[]):
+    print("状态价值：")
+    for i in range(agent.env.nrow):
+        for j in range(agent.env.ncol):
+            # 为了输出美观,保持输出6个字符
+            print('%6.6s' % ('%.3f' % agent.v[i * agent.env.ncol + j]),
+                  end=' ')
+        print()
+
+    print("策略：")
+    for i in range(agent.env.nrow):
+        for j in range(agent.env.ncol):
+            # 一些特殊的状态,例如悬崖漫步中的悬崖
+            if (i * agent.env.ncol + j) in disaster:
+                print('****', end=' ')
+            elif (i * agent.env.ncol + j) in end:  # 目标状态
+                print('EEEE', end=' ')
+            else:
+                a = agent.pi[i * agent.env.ncol + j]
+                pi_str = ''
+                for k in range(len(action_meaning)):
+                    pi_str += action_meaning[k] if a[k] > 0 else 'o'
+                print(pi_str, end=' ')
+        print()
+
+
+# 经过5 次策略评估和策略提升的循环迭代，策略收敛了，此时将获得的策略打印出来。
+# 用贝尔曼最优方程去检验其中每一个状态的价值，可以发现最终输出的策略的确是最优策略。
+
+env = CliffWalkingEnv()
+action_meaning = ['^', 'v', '<', '>']
+theta = 0.001
+gamma = 0.9
+agent = PolicyIteration(env, theta, gamma)
+agent.policy_iteration()
+print_agent(agent, action_meaning, list(range(37, 47)), [47])
