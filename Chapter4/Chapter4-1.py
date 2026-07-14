fd96c02a87107f47495527080cde1039ba7cf884
nibirou <741420370@qq.com>
2026-07-14 09:30:08 +0000

    更新

--- Chapter4/Chapter4-1.py
+++ Chapter4/Chapter4-1.py
@@ -36,3 +36,18 @@ class CliffWalkingEnv:
                     P[i * self.ncol + j][a] = [(1, next_state, reward, done)]
         return P
 
+class PolicyIteration:
+    """ 策略迭代算法 """
+    def __init__(self, env, theta, gamma):
+        self.env = env
+        self.v = [0] * self.env.ncol * self.env.nrow  # 初始化价值为0
+        self.pi = [[0.25, 0.25, 0.25, 0.25] for i in range(self.env.ncol * self.env.nrow)]  # 初始化为均匀随机策略
+        self.theta = theta  # 策略评估收敛阈值
+        self.gamma = gamma  # 折扣因子
+
+    def policy_evaluation(self):  # 策略评估
+        cnt = 1  # 计数器
+        while 1:
+            max_diff = 0
+            new_v = [0] * self.env.ncol * self.env.nrow
+            for s in range(self.env.ncol * self.env.nrow):
