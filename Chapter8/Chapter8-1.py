873771d8d2ab0828a95a222ab4f757b7140e211b
nibirou <741420370@qq.com>
2026-07-26 22:40:36 +0800

    更新

--- Chapter8/Chapter8-1.py
+++ Chapter8/Chapter8-1.py
@@ -32,4 +32,39 @@ class DQN:
                  target_update,
                  device,
                  dqn_type='VanillaDQN'):
-        
+        self.action_dim = action_dim
+        self.q_net = Qnet(state_dim, hidden_dim, self.action_dim).to(device)
+        self.target_q_net = Qnet(state_dim, hidden_dim,
+                                    self.action_dim).to(device)
+        self.optimizer = torch.optim.Adam(self.q_net.parameters(),
+                                            lr=learning_rate)
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.target_update = target_update
+        self.count = 0
+        self.dqn_type = dqn_type
+        self.device = device
+    
+    def take_action(self, state):
+        if np.random.random() < self.epsilon:
+            action = np.random.randint(self.action_dim)
+        else:
+            state = torch.tensor([state], dtype=torch.float).to(self.device)
+            action = self.q_net(state).argmax().item()
+        return action
+    
+    def max_q_value(self, state):
+        state = torch.tensor([state], dtype=torch.float).to(self.device)
+        return self.q_net(state).max().item()
+    
+    def update(self, transition_dict):
+        states = torch.tensor(transition_dict['states'],
+                              dtype=torch.float).to(self.device)
+        actions = torch.tensor(transition_dict['actions']).view(-1, 1).to(
+            self.device)
+        rewards = torch.tensor(transition_dict['rewards'],
+                               dtype=torch.float).view(-1, 1).to(self.device)
+        next_states = torch.tensor(transition_dict['next_states'],
+                                   dtype=torch.float).to(self.device)
+        dones = torch.tensor(transition_dict['dones'],
+                             dtype=torch.float).view(-1, 1).to(self.device)
